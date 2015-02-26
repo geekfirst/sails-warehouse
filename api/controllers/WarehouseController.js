853fb38f553f2a917343143a4bb306fd25963dea
Tim Rinaldo <geekfirst247@gmail.com>
2015-02-26 07:13:15 +0700

    first init

---
+++ api/controllers/WarehouseController.js
@@ -0,0 +1,11 @@
+/**
+ * WarehouseController
+ *
+ * @description :: Server-side logic for managing warehouses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
